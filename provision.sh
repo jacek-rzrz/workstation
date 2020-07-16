61a00d724319ba7490ce3105d4b5ed39da21f955
Jacek Rz <jacek@rzrz.pl>
2020-07-16 13:23:41 +0200

    Add dotfiles folder

--- provision.sh
+++ provision.sh
@@ -3,6 +3,8 @@ echo google-chrome, iterm2, telegram, slack, shiftit, intellij-idea, flycut, vir
   tr , '\n' | \
   while read formula; do brew cask install $formula; done
 
-echo docker, docker-machine, node, awscli |\
+echo docker, docker-machine, node, awscli, jq, watch |\
   tr , '\n' | \
   while read formula; do brew install $formula; done
+
+cp -r dotfiles/. ~
